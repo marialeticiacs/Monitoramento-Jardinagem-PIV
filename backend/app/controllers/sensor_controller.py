17c792925799b65df18a2ed780e6bde2a44191ec
luyzaLuyza <wangmorangao@gmail.com>
2024-06-08 03:44:31 -0300

    adicionando varias coisas na home

--- backend/app/controllers/sensor_controller.py
+++ backend/app/controllers/sensor_controller.py
@@ -4,6 +4,7 @@ from app.models.sensor_data import insert_sensor_data
 from app.models.user_data import update_ultimo_alerta, get_user_by_planta_id
 from app import db
 from datetime import datetime, timedelta
+from bson.json_util import dumps
 
 def receive_data():
     data = request.json
@@ -35,3 +36,38 @@ def receive_data():
 
     insert_sensor_data(planta_id, temperatura, umidade_solo, umidade_ar, alerta, rega)
     return jsonify({"status": "success"}), 201
+
+def get_latest_sensor_data():
+    sensor_data = db.sensor_data.find().sort('data_hora', -1).limit(1)
+    try:
+        data = next(sensor_data)
+        return jsonify({
+            'temperatura': data['temperatura_ambiente'],
+            'umidade_solo': data['umidade_solo']
+        }), 200
+    except StopIteration:
+        return jsonify({'error': 'No data found'}), 404
+
+def get_daily_average(date):
+    start = datetime.strptime(date, "%Y-%m-%d")
+    end = start + timedelta(days=1)
+    average_data = db.sensor_data.aggregate([
+        {"$match": {"data_hora": {"$gte": start, "$lt": end}}},
+        {"$group": {
+            "_id": None,
+            "temperatura_ambiente": {"$avg": "$temperatura_ambiente"},
+            "umidade_solo": {"$avg": "$umidade_solo"},
+            "umidade_ar": {"$avg": "$umidade_ar"}
+        }}
+    ])
+    return jsonify(dumps(average_data))
+
+def get_sensor_data():
+    sensor_data = db.sensor_data.find().sort('data_hora', -1)
+    data = [{
+        'data_hora': entry['data_hora'].isoformat(),
+        'temperatura_ambiente': entry['temperatura_ambiente'],
+        'umidade_solo': entry['umidade_solo'],
+        'umidade_ar': entry['umidade_ar']
+    } for entry in sensor_data]
+    return jsonify(data)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from app.controllers.user_controller import register_user, login_user, get_users, get_user, update_user, delete_user
-from app.controllers.sensor_controller import receive_data
+from app.controllers.sensor_controller import receive_data, get_latest_sensor_data, get_daily_average, get_sensor_data
 from app.controllers.grafico_controller import get_air_humidity_data, get_temperature_data, get_soil_moisture_data
 from flask_jwt_extended import jwt_required
 
@@ -16,6 +16,10 @@ routes.route('/users/<user_id>', methods=['DELETE'])(jwt_required()(delete_user)
 
 # Rota do sensor sem autenticação JWT
 routes.route('/receive_data', methods=['POST'])(receive_data)
+routes.route('/get_latest_sensor_data', methods=['GET'])(get_latest_sensor_data)
+routes.route('/get_daily_average/<date>', methods=['GET'])(get_daily_average)
+routes.route('/get_sensor_data', methods=['GET'])(get_sensor_data)
+
 
 # Rotas para dados de gráficos
 routes.route('/get_temperature_data', methods=['GET'])(get_temperature_data)
